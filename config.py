33f546b464e221ecd8cae1ad203e0156977db6ed
LOL890U <jdhdusiidebbarma@gmail.com>
2024-07-22 20:46:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7299779165:AAHgZItxKJ_Q90L2DNQ2n1OWLShatndxueQ")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@DDOSMAKER")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5338045956")
 
 
 
